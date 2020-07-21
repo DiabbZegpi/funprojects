91828f9cb2094ddf1e09bddb2c41df40b9370e0e
Diabb Zegpi <diabbluis@gmail.com>
2020-07-21 19:30:13 -0400

    Code for mathematical functions and ploting

--- Birthday paradox/Code.R
+++ Birthday paradox/Code.R
@@ -1,6 +1,7 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Packages~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-library(ggplot2)
-library(dplyr)
+library(ggplot2)    # Ploting
+library(dplyr)      # Data manipulation
+library(patchwork)  # Plot arrangement
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Building a function~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 birthday_paradox <- function(n) {
   total_cases <- 365 ^ n
@@ -17,10 +18,17 @@ same_birthday_probs <- vector()
 for(i in seq_len(60)){
   same_birthday_probs[i] <- birthday_paradox(i)
 }
-probs_data <- tibble(people = 1:60, probs = same_birthday_probs)
+
+probs_data <- tibble(people = 1:60, probs = same_birthday_probs) %>% 
+  mutate(odds = probs / (1 - probs),
+         log_odds = log(odds + .001))
+
 geom_data <- probs_data %>% 
   filter(people %in% c(23,57)) %>% 
   bind_cols(xend = c(15,52), yend = c(.55,.80))
+
+interception_data <- probs_data %>% 
+  filter(people == 53)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Ploting~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Theme setup~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 plot_theme <- function(){
@@ -33,7 +41,7 @@ plot_theme <- function(){
 }
 point_colour <- "gray20"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Probability plot~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-birthday_plot <- probs_data %>% 
+probability_plot <- probs_data %>% 
   ggplot(aes(x = people, y = probs)) +
   geom_line(aes(color = probs), size = 2) +
   geom_point(data = geom_data, 
@@ -46,17 +54,41 @@ birthday_plot <- probs_data %>%
   annotate(geom = "text", x = 15, y = .60, 
            fontface = "italic", colour = point_colour,
            label = "50.7% probability\nwith 23 people") +
-  annotate(geom = "text", x = 49, y = .80, 
+  annotate(geom = "text", x = 45, y = .75, 
            fontface = "italic", colour = point_colour,
            label = "99% probability\nwith 57 people") +
   scale_y_continuous(labels = scales::percent_format()) +
   scale_x_continuous(breaks = seq(0,60,10)) +
   scale_color_gradient(low = "#238b45", high = "#7a0177") +
-  labs(title = "Probability of finding at least two people that share birthday",
+  labs(title = "Probability of finding people that share birthday",
        x = "Sample size",
        y = "Probability") +
   plot_theme()
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Odds plot~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+odds_plot <- probs_data %>% 
+  ggplot(aes(x = people, y = odds)) +
+  geom_line(aes(colour = odds), size = 2) +
+  geom_point(data = interception_data,
+             aes(x = people, y = odds),
+             shape = 23, fill = point_colour, size = 4) +
+  geom_segment(data = interception_data,
+               aes(x = people, y = odds, xend = people),
+               yend = 0, linetype = "dashed", colour = point_colour, size = .9) +
+  geom_segment(data = interception_data,
+               aes(x = people, y = odds, yend = odds),
+               xend = 0, linetype = "dashed", colour = point_colour, size = .9) +
+  annotate(geom = "text", x = 24, y = 60, 
+           fontface = "italic", colour = point_colour,
+           label = "Take 50 to 1 if there are\nless than 53 people!") +
+  scale_x_continuous(breaks = seq(0,60,10)) +
+  scale_color_gradient(low = "#238b45", high = "#7a0177") +
+  labs(x = "Sample size", y = "Odds of winning the bet",
+       title = "Do you wanna bet? Let's see the odds") +
+  plot_theme() 
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Final plot~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+birthday_plot <- probability_plot + odds_plot
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Saving files~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ggsave("Birthday paradox/birthday_plot.png", plot = birthday_plot, 
-       type = "cairo", dpi = 1200)
+       type = "cairo", dpi = 1200, width = 9, height = 5.5)
 readr::write_csv(probs_data, path = "Birthday paradox/probs_data.csv")
